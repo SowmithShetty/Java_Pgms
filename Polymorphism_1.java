195cc1f974876f7bafdba87ed95812fc2e3ff8e3
SowmithShetty <sowmithshetty01152@gmail.com>
2025-04-19 12:14:37 +0530

    Add files via upload

---
+++ Polymorphism_1.java
@@ -0,0 +1,36 @@
+class A
+{ 
+    int i,j;    
+    A(int a,int b)
+    {
+        i=a;
+        j=b;
+    }
+void show()
+{
+    System.out.println("I am parent");
+}
+    
+}
+class B extends A{
+    int k;
+    B(int a,int b,int c)
+    {
+        super(a,b);
+        k=c;
+    }
+    void show()
+    {
+        System.out.println("I am child");
+    }
+
+}
+class Polymorphism_1
+{
+    public static void main(String[] args) {
+        A a1;
+        B b1=new B(1,2,3);
+        a1=b1;
+        a1.show();
+}
+}
